34be9ef0949a82c51877cf1bc5c76e2ff4a2523e
Michael Bayne <mdb@samskivert.com>
2025-01-13 14:25:26 -0800

    Setting up CloudKit syncing.
    Maybe?

--- InputOutput/Reading/ReadItem.swift
+++ InputOutput/Reading/ReadItem.swift
@@ -9,7 +9,7 @@ enum ReadType: String, Codable, CaseIterable, Identifiable, CustomStringConverti
 
   var id: Self { self }
   var label: String { rawValue }
-  var description :String { rawValue }
+  var description: String { rawValue }
 }
 
 struct ReadJson: Decodable {
@@ -21,7 +21,7 @@ struct ReadJson: Decodable {
   var tags: [String]?
   var link: String?
   var rating: String?
-  var started :String?
+  var started: String?
   var completed: String?
   var recommender: String?
   var abandoned: Bool?
@@ -31,35 +31,36 @@ extension SchemaV1 {
   @Model
   class ReadItem: Identifiable, Consumable {
     // Item properties
-    var id: UUID
-    var created: Date
-    var tags: [Tag]
+    var id: UUID = UUID()
+    var created: Date = Date.now
+    var tags: [Tag] = []
     var link: String?
     var started: Date?
     var completed: Date?
-    
+
     // Consumable properties
     var rating: Rating?
     var recommender: String?
-    
+
     // ReadItem properties
-    var format: ReadType
-    var title: String
+    var format: ReadType = ReadType.book
+    var title: String = ""
     var author: String?
-    var abandoned: Bool
-    
-    init(id: UUID = UUID(),
-         created: Date,
-         tags: [Tag] = [],
-         link: String? = nil,
-         started: Date? = nil,
-         completed: Date? = nil,
-         rating: Rating = .none,
-         recommender: String? = nil,
-         format: ReadType,
-         title: String,
-         author: String? = nil,
-         abandoned: Bool = false
+    var abandoned: Bool = false
+
+    init(
+      id: UUID = UUID(),
+      created: Date,
+      tags: [Tag] = [],
+      link: String? = nil,
+      started: Date? = nil,
+      completed: Date? = nil,
+      rating: Rating = .none,
+      recommender: String? = nil,
+      format: ReadType,
+      title: String,
+      author: String? = nil,
+      abandoned: Bool = false
     ) {
       self.id = id
       self.created = created
@@ -74,13 +75,12 @@ extension SchemaV1 {
       self.author = author
       self.abandoned = abandoned
     }
-    
+
     func isProtracted() -> Bool { true }
     func startable() -> Bool { started != nil }
-    
+
     func ratingIcon() -> String? {
-      if abandoned { "😴" }
-      else { rating.map({ $0.emoji }) }
+      if abandoned { "😴" } else { rating.map({ $0.emoji }) }
     }
   }
 }
@@ -89,10 +89,19 @@ typealias ReadItem = SchemaV1.ReadItem
 
 var testReadItems: [ReadItem] {
   [
-    ReadItem(created: .now.addingTimeInterval(-10), started: .now, recommender: "Some guy", format: .book, title: "The Cat in the Hat", author: "Dr. Seuss"),
-    ReadItem(created: .now.addingTimeInterval(-20), link: "https://samskivert.com/", format: .audiobook, title: "One Flew Over the Cuckoo's Nest", author: "Ken Kesey"),
-    ReadItem(created: .now.addingTimeInterval(-30), format: .article, title: "A Brief History of Chickens"),
-    ReadItem(created: .now.addingTimeInterval(-40), recommender: "Mom", format: .book, title: "The Great Gatsby", author: "F. Scott Fitzgerald"),
-    ReadItem(created: .now.addingTimeInterval(-50), started: .now.addingTimeInterval(-20), completed: .now, rating: .good, format: .paper, title: "Goto Considered Harmful", author: "Edsgar Dijkstra"),
+    ReadItem(
+      created: .now.addingTimeInterval(-10), started: .now, recommender: "Some guy", format: .book,
+      title: "The Cat in the Hat", author: "Dr. Seuss"),
+    ReadItem(
+      created: .now.addingTimeInterval(-20), link: "https://samskivert.com/", format: .audiobook,
+      title: "One Flew Over the Cuckoo's Nest", author: "Ken Kesey"),
+    ReadItem(
+      created: .now.addingTimeInterval(-30), format: .article, title: "A Brief History of Chickens"),
+    ReadItem(
+      created: .now.addingTimeInterval(-40), recommender: "Mom", format: .book,
+      title: "The Great Gatsby", author: "F. Scott Fitzgerald"),
+    ReadItem(
+      created: .now.addingTimeInterval(-50), started: .now.addingTimeInterval(-20), completed: .now,
+      rating: .good, format: .paper, title: "Goto Considered Harmful", author: "Edsgar Dijkstra"),
   ]
 }
