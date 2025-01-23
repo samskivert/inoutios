8cac373c6b9ccf91069b2e521eb75f85a2e668db
Michael Bayne <mdb@samskivert.com>
2025-01-22 17:10:59 -0800

    Fix Icon preview.

--- InputOutput/IconPreview.swift
+++ InputOutput/IconPreview.swift
@@ -1,9 +1,21 @@
 import SwiftUI
 
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
 struct IconPreview: View {
     var body: some View {
-      HStack {
-        ForEach(Icon.allCases) { icon($0) }
+      VStack {
+        ForEach(Icon.allCases.chunked(into: 7), id: \.self) { row in
+          HStack {
+            ForEach(row) { icon($0) }
+          }
+        }
       }
     }
 }
